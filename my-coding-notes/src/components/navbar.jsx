c4750d3c39f879e408420b1bcadd1150237a0274
Paul <115771869+Polypheus@users.noreply.github.com>
2025-03-18 09:41:51 +0000

    design

--- my-coding-notes/src/components/navbar.jsx
+++ my-coding-notes/src/components/navbar.jsx
@@ -4,11 +4,11 @@ import '../App.css';
 
 const Navbar = () => {
   return (
-    <nav className=' p-5 bg-gray-800 text-white position-sticky top-0'>
-      <ul className='flex space-x-5'>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
+    <nav className='p-5 bg-gray-800 position-sticky top-0'>
+      <ul className='grid justify-items-end gap-1 h-30  text-white'>
+        <li className=' h-60 w-50 bg-gray-900'><a href="/Home" className='flex h-full w-full justify-center items-center text-4xl'>Home</a></li>
+        <li  className=' h-60 w-50 bg-gray-900'><a href="/about" className='flex h-full w-full justify-center items-center text-4xl'>About</a></li>
+        <li  className=' h-60 w-50 bg-gray-900'><a href="/contact" className='flex h-full w-full justify-center items-center text-4xl'>Contacts</a></li>
       </ul>
     </nav>
   );
--- my-coding-notes/src/main.jsx
+++ my-coding-notes/src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home from './pages/home.jsx'
+import About from './pages/about.jsx'
 import {
   createBrowserRouter,
   RouterProvider,
@@ -12,13 +14,19 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
   }
 ]);
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
